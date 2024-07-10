567c9857a723333c2b9d8c524dc153ccb0510576
Akil <akilakil6874@gmail.com>
2024-07-10 14:11:46 +0530

    Added Quiz Question

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
-import Image from "next/image";
+import Button from "@/components/Button";
+import DropOptions from "@/components/DropDown";
 
 export default function Home() {
   return (
@@ -7,25 +8,29 @@ export default function Home() {
         <span className="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">
           Welcome to
         </span>{" "}
-        Quiz.
+        quiz...
       </h1>
-      <section className="p-10 my-10 rounded-lg shadow-xl w-{65%}">
+      <section className="p-10 my-10 rounded-lg shadow-xl w-1/2 bg-stone-50">
         <div>
           <label
-            htmlFor="first_name"
+            htmlFor="questions"
             className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
           >
-            No of Questions
+            Number of Questions
           </label>
           <input
             type="number"
             defaultValue={10}
             min={0}
             max={20}
-            id="first_name"
+            id="questions"
             className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
             required
           />
+        </div>
+        <div className="w-full flex flex-col justify-center items-center">
+          <DropOptions />
+          <Button />
         </div>
       </section>
     </section>
---
+++ components/DropDown/index.tsx
@@ -0,0 +1,54 @@
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
+export default function DropOptions() {
+  return (
+    <section className="flex justify-evenly items-center py-5 w-full">
+      <div className="px-7 py-4 w-1/3 mx-4">
+        <DropdownMenu>
+          <DropdownMenuTrigger className="flex outline-none justify-between w-full px-10 py-3 rounded-lg shadow-lg hover:bg-blue-600 hover:text-gray-100">Select Category</DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <DropdownMenuLabel>Select Category</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>Profile</DropdownMenuItem>
+            <DropdownMenuItem>Billing</DropdownMenuItem>
+            <DropdownMenuItem>Team</DropdownMenuItem>
+            <DropdownMenuItem>Subscription</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </div>
+      <div className="px-7 py-4 w-1/3 mx-4">
+        <DropdownMenu>
+          <DropdownMenuTrigger className="flex outline-none justify-between w-full px-10 py-3 rounded-lg shadow-lg hover:bg-blue-600 hover:text-gray-100">Select Level</DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <DropdownMenuLabel>Select Level</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>Profile</DropdownMenuItem>
+            <DropdownMenuItem>Billing</DropdownMenuItem>
+            <DropdownMenuItem>Team</DropdownMenuItem>
+            <DropdownMenuItem>Subscription</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </div>
+      <div className="px-7 py-4 w-1/3 mx-4">
+        <DropdownMenu>
+          <DropdownMenuTrigger className="flex outline-none justify-between w-full px-10 py-3 rounded-lg shadow-lg hover:bg-blue-600 hover:text-gray-100">Select Type</DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <DropdownMenuLabel>Select Type</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>Profile</DropdownMenuItem>
+            <DropdownMenuItem>Billing</DropdownMenuItem>
+            <DropdownMenuItem>Team</DropdownMenuItem>
+            <DropdownMenuItem>Subscription</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </div>
+    </section>
+  );
+}
