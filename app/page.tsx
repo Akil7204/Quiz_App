c0dbd236684b9bde4fa1b426c3c7ec3ef63a92f5
Akil <akilakil6874@gmail.com>
2024-07-10 10:13:14 +0530

    dropdown and home page created

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,32 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <h1>A=hello</h1>
+    <section className="flex flex-col justify-center items-center my-10">
+      <h1 className="mb-4 text-3xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl">
+        <span className="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">
+          Welcome to
+        </span>{" "}
+        Quiz.
+      </h1>
+      <section className="p-10 my-10 rounded-lg shadow-xl w-{65%}">
+        <div>
+          <label
+            htmlFor="first_name"
+            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+          >
+            No of Questions
+          </label>
+          <input
+            type="number"
+            defaultValue={10}
+            min={0}
+            max={20}
+            id="first_name"
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            required
+          />
+        </div>
+      </section>
+    </section>
   );
 }
